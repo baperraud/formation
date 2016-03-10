aed72261ef459cf861ed1a849c77b1841b426784
baperraud <b.perraud@dreamcentury.com>
2016-03-10 15:04:38 +0100

    Creation de updateComment.php

---
+++ App/Frontend/Modules/News/Views/updateComment.php
@@ -0,0 +1,12 @@
+<?php
+/** @var string $form */
+?>
+
+<h2>Modifier un commentaire</h2>
+<form action="" method="post">
+	<p>
+		<?= $form ?>
+
+		<input type="submit" value="Modifier" />
+	</p>
+</form>
