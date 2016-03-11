931ba3514bab3d087983b18ec6a0f60171283ac6
baperraud <b.perraud@dreamcentury.com>
2016-03-11 17:47:24 +0100

    Correction syntaxe

--- App/Frontend/Modules/User/Views/index.php
+++ App/Frontend/Modules/User/Views/index.php
@@ -1,15 +1,3 @@
-<!--<h2>Connexion</h2>-->
-<!---->
-<!--<form action="" method="post">-->
-<!--	<label for="login">Pseudo</label>-->
-<!--	<input type="text" id="login" name="login" /><br />-->
-<!---->
-<!--	<label for="password">Mot de passe</label>-->
-<!--	<input type="password" id="password" name="password" /><br /><br />-->
-<!---->
-<!--	<input type="submit" value="Connexion" />-->
-<!--</form>-->
-
 <?php
 /** @var string $form */
 ?>
